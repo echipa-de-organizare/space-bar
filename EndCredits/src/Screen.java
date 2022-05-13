19c35311fe5f6dd6a032737304207ed90132780c
917_Zachman_Roxana <zrie3047@scs.ubbcluj.ro>
2022-05-13 17:42:37 +0300

    Changed credits text and wrote to next planet file

--- EndCredits/src/Screen.java
+++ EndCredits/src/Screen.java
@@ -5,8 +5,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Screen extends JPanel implements ActionListener {
     Timer creditTimer = new Timer(0, this);
@@ -26,16 +28,25 @@ public class Screen extends JPanel implements ActionListener {
 
         this.setBackground(Color.BLACK);
         text = """
-                Credits:
-
-                Music:
-                Lala1
-                Lala2
-                Lala3
-                Lala4
-
-
-                Lala5""";
+                Credits
+                                
+                                
+                --space bar--
+                                
+                Fiicode Gamedev 2022
+                                
+                Alexandru Negru
+                Roxana Zachman
+                Samuel Gherasim
+                                
+                Babeș-Bolyai University, Cluj-Napoca, and Technical University of Cluj-Napoca
+                                
+                "echipa de organizare"
+                                
+                                
+                                
+                All rights reserved.
+                """;
         repaint();
         creditTimer.start();
         device.setFullScreenWindow(window);
@@ -44,7 +55,7 @@ public class Screen extends JPanel implements ActionListener {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setFont(new Font("Arial", Font.PLAIN, 50));
+        g2d.setFont(new Font("Candara", Font.PLAIN, 50));
         g2d.setColor(Color.WHITE);
         g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         int y = textY;
@@ -58,14 +69,26 @@ public class Screen extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 //        System.out.println(textY);
-//        textY -= 5;
-        textY -= 6;
-        if (textY < -650) {
+        textY -= 5;
+//        textY -= 6;
+        if (textY < -1200) {
             creditTimer.stop();
             BufferedWriter writer;
             try {
+                File file = new File(FileSystemView.getFileSystemView().getDefaultDirectory().getPath() + "\\spacebarlogS.txt");
+                Scanner scanner = new Scanner(file);
+                int state = 0;
+                if (scanner.hasNextLine()) {
+                    state = Integer.parseInt(scanner.nextLine());
+                }
+                scanner.close();
+
                 writer = new BufferedWriter(new FileWriter(FileSystemView.getFileSystemView().getDefaultDirectory().getPath() + "\\spacebarlogP.txt"));
-                writer.write("1");
+                if (state == 1) {
+                    writer.write("1");
+                } else if (state == 3) {
+                    writer.write("-1");
+                }
                 writer.close();
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
