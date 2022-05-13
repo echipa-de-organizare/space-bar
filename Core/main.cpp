86c109a0c098e24527bf0609e349c5d80aac20ff
917_Zachman_Roxana <zrie3047@scs.ubbcluj.ro>
2022-05-13 13:09:58 +0300

    Moved core exe outside and named it spacebar.exe

--- Core/main.cpp
+++ Core/main.cpp
@@ -55,19 +55,19 @@ void handle_files()
 }
 void hardcode_planets()
 {
-    planets.insert(std::pair<int, std::string>(0, "..\\..\\Release Bar\\Bar\\Bar.exe"));
-    planets.insert(std::pair<int, std::string>(1, "..\\..\\Release Travel\\Travel\\Travel.exe"));
-    planets.insert(std::pair<int, std::string>(2, "..\\..\\Radio planet\\tcp-planet\\main.exe"));
-    planets.insert(std::pair<int, std::string>(3, "..\\..\\Planet\\planet.exe"));
-    planets.insert(std::pair<int, std::string>(5, "..\\..\\Earth\\earth.exe"));
-    planets.insert(std::pair<int, std::string>(6, "..\\..\\EndCredits\\EndCredits.jar"));
+    planets.insert(std::pair<int, std::string>(0, "Release Bar\\Bar\\Bar.exe"));
+    planets.insert(std::pair<int, std::string>(1, "Release Travel\\Travel\\Travel.exe"));
+    planets.insert(std::pair<int, std::string>(2, "Radio planet\\tcp-planet\\main.exe"));
+    planets.insert(std::pair<int, std::string>(3, "End Planet\\EndPlanet\\main.exe"));
+    planets.insert(std::pair<int, std::string>(5, "Earth\\earth.exe"));
+    planets.insert(std::pair<int, std::string>(6, "EndCredits\\EndCredits.jar"));
 
     std::string empty;
     working_directories.insert(std::pair<int, std::string>(0, empty));
     working_directories.insert(std::pair<int, std::string>(1, empty));
-    working_directories.insert(std::pair<int, std::string>(2, "..\\..\\Radio planet\\tcp-planet"));
-    working_directories.insert(std::pair<int, std::string>(3, "..\\..\\Planet"));
-    working_directories.insert(std::pair<int, std::string>(5, "..\\..\\Earth"));
+    working_directories.insert(std::pair<int, std::string>(2, "Radio planet\\tcp-planet"));
+    working_directories.insert(std::pair<int, std::string>(3, "End Planet\\EndPlanet"));
+    working_directories.insert(std::pair<int, std::string>(5, "Earth"));
     working_directories.insert(std::pair<int, std::string>(6, empty));
 }
 std::string get_planet_path(int id)
@@ -126,7 +126,6 @@ int main()
 
     hardcode_planets();
     next_planet_id = read_planet_id();
-//    reset_time();
     start_planet(next_planet_id);
     while (true)
     {
@@ -134,7 +133,6 @@ int main()
         int state = read_state();
         if (id == -1)
         {
-            std::cout << "lalal";
             reset_planet_id();
             break;
         }
