edced1ac9fa400cd309b80f420850ef60682a68d
Samuel Gherasim <sami.gherasim@gmail.com>
2022-05-27 21:11:06 +0300

    DLCHandler implemented

---
+++ DLCHandler/cmake-build-debug/CMakeFiles/DLCHandler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DLCHandler.dir/main.cpp.obj"
+  "DLCHandler.exe"
+  "DLCHandler.exe.manifest"
+  "DLCHandler.pdb"
+  "libDLCHandler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DLCHandler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
